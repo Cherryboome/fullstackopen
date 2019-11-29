801bf27eda2178f7731ee4edd65ce372323642f5
Cherryboome <muhamidifarian@yahoo.com>
2019-11-29 09:47:16 +0200

    completed exercise 2.2 Course contents step7

--- part2/part2-1/src/components/Course.js
+++ part2/part2-1/src/components/Course.js
@@ -3,12 +3,20 @@ import Part from "./Part";
 
 const Course = ({ course }) => {
   const { name, parts } = course;
+
+  const sum = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+
   return (
     <div>
       <h1>{name}</h1>
       {parts.map(part => (
         <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
+      <div>
+        <h4>total of {sum} exercises</h4>
+      </div>
     </div>
   );
 };
