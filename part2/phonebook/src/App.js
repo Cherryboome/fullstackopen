a4d885d0591e96e114b3ef7c8c25d33f3ea1d219
Cherryboome <muhamidifarian@yahoo.com>
2019-12-02 13:12:29 +0200

    completed exercise 2.9 The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" }
+    { name: "Arto Hellas", number: "040-123-4567" },
+    { name: "Ada Lovelace", number: "639-444-5323" },
+    { name: "Dan Abramov", number: "126-430-2343" },
+    { name: "Mary Poppendieck", number: "239-232-6423" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   const addPerson = event => {
     event.preventDefault();
@@ -27,15 +32,24 @@ const App = () => {
     }
   };
 
-  const handleNameChange = event => {
-    setNewName(event.target.value);
+  const handleNameChange = event => setNewName(event.target.value);
+
+  const handleNumberChange = event => setNewNumber(event.target.value);
+
+  const handleSearchChange = event => {
+    setNewSearch(event.target.value);
+    setShowAll(false);
   };
 
-  const handleNumberChange = event => {
-    setNewNumber(event.target.value);
-  };
+  const searchResults = showAll
+    ? persons
+    : persons.filter(person => {
+        let filteredName = person.name.toLocaleLowerCase();
+        let searchName = newSearch.toLowerCase();
+        return filteredName.includes(searchName);
+      });
 
-  const list = persons.map(person => {
+  const list = searchResults.map(person => {
     return (
       <p key={person.name}>
         {person.name} {person.number}
@@ -46,6 +60,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search the phonebook:{" "}
+        <input value={newSearch} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new listing</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} required />
