023850cfb778c6fd6223a918d785a1364b77637c
Cherryboome <muhamidifarian@yahoo.com>
2019-12-17 19:39:37 +0200

    4.4: helper functions and unit tests, step2

--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = array => {
   }
 }
 
+const totalLikes = array => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  return array.map(item => item.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
