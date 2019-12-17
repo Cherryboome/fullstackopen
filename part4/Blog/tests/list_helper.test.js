c59fd280c9ae3e5330392639aebc56c3fecf87f4
Cherryboome <muhamidifarian@yahoo.com>
2019-12-17 21:43:00 +0200

    completed exercise 4.5*: helper functions and unit tests, step3

--- part4/Blog/tests/list_helper.test.js
+++ part4/Blog/tests/list_helper.test.js
@@ -87,3 +87,38 @@ describe('total likes', () => {
     expect(result).toBe(36)
   })
 })
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('one blog post has most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({ title: 'TDD harms architecture', author: 'Robert C. Martin', likes: 12 })
+  })
+
+})
--- part4/Blog/utils/list_helper.js
+++ part4/Blog/utils/list_helper.js
@@ -12,7 +12,15 @@ const totalLikes = array => {
   return array.map(item => item.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = array => {
+  const favorite = array.sort((a, b) => b.likes - a.likes)
+
+  return { title: favorite[0].title, author: favorite[0].author, likes: favorite[0].likes }
+
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
