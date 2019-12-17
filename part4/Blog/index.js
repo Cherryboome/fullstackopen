08f1a2342b4a58581bd793def1e698bf8607a55a
Cherryboome <muhamidifarian@yahoo.com>
2019-12-17 15:19:05 +0200

    completed exercise 4.2 Blog list, step2 refactoring blog app

--- part4/Blog/index.js
+++ part4/Blog/index.js
@@ -1,58 +1,9 @@
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
 const config = require("./utils/config");
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
+const server = http.createServer(app);
 
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-// const mongoUrl = "mongodb://localhost/bloglist";
-mongoose
-  .connect(config.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(result => {
-    console.log("connected to MongoDB");
-  })
-  .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
