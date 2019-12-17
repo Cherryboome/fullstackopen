b505fb7b270a476ad0ae83fe83fabddc4e4fa67d
Cherryboome <muhamidifarian@yahoo.com>
2019-12-17 19:06:38 +0200

    completed exercise 4.3: helper functions and unit tests, step1

--- part4/Blog/app.js
+++ part4/Blog/app.js
@@ -1,12 +1,12 @@
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const mongoose = require("mongoose");
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
 
-console.log("connecting to", config.MONGODB_URI);
+console.log('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -14,15 +14,15 @@ mongoose
     useNewUrlParser: true
   })
   .then(result => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
-app.use("/api/blogs", blogsRouter);
+app.use('/api/blogs', blogsRouter)
 
-module.exports = app;
+module.exports = app
---
+++ part4/Blog/utils/list_helper.js
@@ -0,0 +1,9 @@
+const dummy = array => {
+  if(Array.isArray(array)) {
+    return 1
+  }
+}
+
+module.exports = {
+  dummy
+}
