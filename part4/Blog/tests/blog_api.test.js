087d883fe8ab5b17f39e99a3c28b658aec16aab2
Cherryboome <muhamidifarian@yahoo.com>
2020-01-27 22:09:10 +0200

    refactored code to optimize tests for exercise 4.8 & 4.9

--- part4/Blog/tests/blog_api.test.js
+++ part4/Blog/tests/blog_api.test.js
@@ -2,8 +2,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Front end web development in 2020',
+    author: 'Lee Wang',
+    url: 'N/A',
+    likes: 30
+  },
+  {
+    title: 'Technologies every developer should know in 2020',
+    author: 'Jean Defaux',
+    url: 'N/A',
+    likes: 25
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  initialBlogs.forEach(async blog => {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  })
+})
 
 describe('return correct amount of blog posts in JSON format', () => {
   test('blogs are returned as json', async () => {
@@ -16,7 +40,7 @@ describe('return correct amount of blog posts in JSON format', () => {
   test('there is one blog', async () => {
     const response = await api.get('/api/blogs')
 
-    expect(response.body.length).toBe(1)
+    expect(response.body.length).toBe(initialBlogs.length)
   })
 })
 
